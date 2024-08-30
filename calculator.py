45134afa1d572d48287b041e06c4199195e2f78f
emawit <arsemaermiyas553@gmail.com>
2024-08-30 05:12:36 -0700

    calculator
    it involves basic arithmetic operation like addition, subtraction, multiplication , division. so please check out!

--- calculator.py
+++ calculator.py
@@ -2,17 +2,15 @@ while True:
     # taking input from the user
     num1=int(input("num1: - "))
     num2=int(input("num2: - "))
-    opreation=input("opreation: - ")
-    # making opreation
+    opreation=input("opreation:_")
+    # making calculation
     if opreation=="+":
-        print(num1+num2)
+      print(num1+num2)
     elif opreation== "-":
-        print(num1-num2)
+      print(num1-num2)
     elif opreation=="*":
-        print(num1*num2)
+      print(num1*num2)
     elif opreation=="/":
-        print(num1/num2)
+      print(num1/num2)
     else:
-        print("invalid")
-
-    
+       ("invalid")
